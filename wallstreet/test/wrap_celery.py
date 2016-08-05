ebe02161e0850beba0366c5d44895aa038728d79
breakhearts <lxx1234@gmail.com>
2016-08-05 20:13:36 +0800

    =

--- wallstreet/test/wrap_celery.py
+++ wallstreet/test/wrap_celery.py
@@ -1,5 +1,6 @@
 from wallstreet import config
 from wallstreet.tasks.celery import app
+from wallstreet.storage import create_sql_engine_and_session_cls
 
 engine, Session = create_sql_engine_and_session_cls(config.get_test("storage", "url"))
 
