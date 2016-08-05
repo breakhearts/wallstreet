0fe04240a417118a64876eb7e0008d534291427f
breakhearts <lxx1234@gmail.com>
2016-08-06 01:54:30 +0800

    =

--- wallstreet/bin/__main__.py
+++ wallstreet/bin/__main__.py
@@ -3,13 +3,16 @@ import sys
 from wallstreet.tasks.stock_history_tasks import update_all_stock_info, update_all_stock_day
 
 parser = argparse.ArgumentParser(description="wallstreet console")
-parser.add_argument('--info', dest="update", action='store_true', const=update_all_stock_day, default=update_all_stock_info )
+parser.add_argument('--info', dest="info", action='store_true')
+parser.add_argument('--day', dest="day", action='store_true')
 args = parser.parse_args(sys.argv[1:])
 
 
 def main():
-    args.update()
-
+    if args.info:
+        update_all_stock_info()
+    elif args.day:
+        update_all_stock_day()
 
 if __name__ == "__main__":
     main()
