6ed3c56535b5b066bc1f17f3ae30f6372db13919
breakhearts <lxx1234@gmail.com>
2016-08-05 17:06:30 +0800

    =reload is only support python2

--- wallstreet/test/wrap_celery.py
+++ wallstreet/test/wrap_celery.py
@@ -1,7 +1,11 @@
 from wallstreet import config
 config.set_config("storage", "url", config.get_test("storage", "url"))
 import wallstreet.tasks.celery
-reload(wallstreet.tasks.celery)
+try:
+	import importlib
+	importlib.reload(wallstreet.tasks.celery)
+except:
+	reload(wallstreet.tasks.celery)
 from wallstreet.tasks.celery import app, engine, Session
 app.conf.update(
     CELERY_ALWAYS_EAGER=True
