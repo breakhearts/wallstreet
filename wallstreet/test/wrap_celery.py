bb4e7434f89d8fe5fb7a2c285416c37846fc56cb
breakhearts <lxx1234@gmail.com>
2016-08-05 16:30:35 +0800

    =reload celery

--- wallstreet/test/wrap_celery.py
+++ wallstreet/test/wrap_celery.py
@@ -2,7 +2,7 @@ from wallstreet import config
 config.set_config("storage", "url", config.get_test("storage", "url"))
 import wallstreet.tasks.celery
 reload(wallstreet.tasks.celery)
-from wallstreet.tasks.celery import app, engine
+from wallstreet.tasks.celery import app, engine, Session
 app.conf.update(
     CELERY_ALWAYS_EAGER=True
 )
