7997d4f025a71fd8db9b8b0a1174edc1be08a794
breakhearts <lxx1234@gmail.com>
2016-08-31 13:58:02 +0800

    celery use fork, so you should init database connections in process init event

--- wallstreet/tasks/celery.py
+++ wallstreet/tasks/celery.py
@@ -5,17 +5,45 @@ from wallstreet import config
 from logging.config import dictConfig
 from wallstreet.logging import config as log_config
 from wallstreet.tasks.task_monitor import task_faiure_recorder
+from celery.signals import worker_process_init
 
 dictConfig(log_config.CELERY_LOGGING)
 app = Celery("task")
 app.config_from_object("wallstreet.tasks.config")
 
-engine, Session = create_sql_engine_and_session_cls(config.get("storage", "url"))
-create_sql_table(engine)
+
+class EngineSession(object):
+    def __init__(self):
+        self.__engine = None
+        self.__Session = None
+
+    @property
+    def Session(self):
+        return self.__Session
+
+    @property
+    def engine(self):
+        return self.__engine
+
+    def init(self, url):
+        self.__engine, self.__Session = create_sql_engine_and_session_cls(url)
+        create_sql_table(self.__engine)
+
+
+engine_Session = EngineSession()
+
+if app.conf.CELERY_ALWAYS_EAGER:
+    engine_Session.init(config.get("storage", "url"))
+
+
+@worker_process_init.connect
+def worker_process_init_handler(sender=None, instance=None, **kwargs):
+    engine_Session.init(config.get("storage", "url"))
 
 
 class RecordFailureTask(Task):
     abstract = True
+
     def on_failure(self, exc, task_id, args, kwargs, einfo):
         if einfo:
             traceback = str(einfo)
--- wallstreet/test/test_stock_task.py
+++ wallstreet/test/test_stock_task.py
@@ -9,16 +9,16 @@ from wallstreet.tasks.storage_tasks import *
 from datetime import datetime, timedelta
 from wallstreet.storage import *
 from wallstreet import base
-from wallstreet.test.wrap_celery import engine, Session
+from wallstreet.test.wrap_celery import engine_Session
 import shutil
 
 
 @pytest.fixture(scope="module")
 def engine_and_session_cls(request):
-    create_sql_table(engine)
+    create_sql_table(engine_Session.engine)
 
     def teardown():
-        drop_sql_table(engine)
+        drop_sql_table(engine_Session.engine)
         if os.path.exists(config.get_path("sec", "data_dir")):
             shutil.rmtree(config.get_path("sec", "data_dir"))
     request.addfinalizer(teardown)
@@ -26,21 +26,21 @@ def engine_and_session_cls(request):
 
 def test_get_all_stock_history(engine_and_session_cls):
     # insert data
-    storage = StockInfoSqlStorage(engine, Session)
+    storage = StockInfoSqlStorage(engine_Session.engine, engine_Session.Session)
     storage.save(base.StockInfo("BIDU", "nasdaq"))
     storage.save(base.StockInfo("BABA", "nasdaq"))
-    storage = LastUpdateSqlStorage(engine, Session)
+    storage = LastUpdateSqlStorage(engine_Session.engine, engine_Session.Session)
     storage.save("BIDU", datetime.today() - timedelta(days=7), LastUpdateStorage.STOCK_DAY)
     update_all_stock_day()
     # check data
-    storage = StockDaySqlStorage(engine, Session)
+    storage = StockDaySqlStorage(engine_Session.engine, engine_Session.Session)
     t = storage.load("BIDU")
     assert len(t) > 0
     assert t[0].symbol == "BIDU"
     t = storage.load("BABA")
     assert len(t) > 0
     assert t[0].symbol == "BABA"
-    storage = LastUpdateSqlStorage(engine, Session)
+    storage = LastUpdateSqlStorage(engine_Session.engine, engine_Session.Session)
     t = storage.load('BABA', LastUpdateStorage.STOCK_DAY)
     assert t > datetime(2016, 1, 1)
 
@@ -49,17 +49,17 @@ def test_update_stock_base_index(engine_and_session_cls):
     stock_days = []
     for i in range(40):
         stock_days.append(base.StockDay("FB", datetime(2005, 1, 1) + timedelta(days=i * 2), 1, 1, 1, 1, 1, 1))
-    stock_day_storage = StockDaySqlStorage(engine, Session)
+    stock_day_storage = StockDaySqlStorage(engine_Session.engine, engine_Session.Session)
     stock_day_storage.save(stock_days)
-    stock_info_storage = StockInfoSqlStorage(engine, Session)
+    stock_info_storage = StockInfoSqlStorage(engine_Session.engine, engine_Session.Session)
     stock_info_storage.save(base.StockInfo("FB", "nasdaq"))
     update_all_stock_base_index()
-    base_index_storage = BaseIndexSqlStorage(engine, Session)
+    base_index_storage = BaseIndexSqlStorage(engine_Session.engine, engine_Session.Session)
     t = base_index_storage.load("FB")
     assert len(t) == 39
     t = base_index_storage.load_last(symbol="FB", limit=1)
     assert len(t) == 1
-    last_update_storage = LastUpdateSqlStorage(engine, Session)
+    last_update_storage = LastUpdateSqlStorage(engine_Session.engine, engine_Session.Session)
     last_update_date = last_update_storage.load("FB", LastUpdateStorage.STOCK_BASE_INDEX)
     assert last_update_date == datetime(2005, 1, 1) + timedelta(days=39 * 2)
     assert t[0].vol60 == 0
@@ -75,14 +75,14 @@ def test_update_stock_info(engine_and_session_cls):
 
 def test_update_all_stock_info(engine_and_session_cls):
     update_all_stock_info()
-    storage = StockInfoSqlStorage(engine, Session)
+    storage = StockInfoSqlStorage(engine_Session.engine, engine_Session.Session)
     t = storage.load_all()
     assert len(t) > 1000
 
 
 def test_update_year_fiscal(engine_and_session_cls):
     get_all_stock_year_fiscal(["BIDU", "AAPL"])
-    storage = RawYearFiscalReportSqlStorage(engine, Session)
+    storage = RawYearFiscalReportSqlStorage(engine_Session.engine, engine_Session.Session)
     t = storage.load("AAPL")
     assert len(t) > 0
     t = storage.load("BIDU")
@@ -94,16 +94,16 @@ def test_update_year_fiscal(engine_and_session_cls):
 
 def test_update_stock_info_details(engine_and_session_cls):
     get_all_stock_info_details(["BIDU", "AAPL"])
-    storage = StockInfoDetailSqlStorage(engine, Session)
+    storage = StockInfoDetailSqlStorage(engine_Session.engine, engine_Session.Session)
     t = storage.load("AAPL")
     assert t.symbol == "AAPL"
 
 
 def test_update_sec_fillings(engine_and_session_cls):
-    storage = SECFillingSqlStorage(engine, Session)
+    storage = SECFillingSqlStorage(engine_Session.engine, engine_Session.Session)
     storage.save(
         base.SECFilling(company_name="Facebook", cik="1326801", form_type="8-K", date=datetime(2015, 1, 31), id="0001326801-16-000060"))
-    storage = StockInfoDetailSqlStorage(engine, Session)
+    storage = StockInfoDetailSqlStorage(engine_Session.engine, engine_Session.Session)
     storage.save(base.StockInfoDetail("FB", "NASDAQ", "0001326801", "XX", "sector", "siccode", "city"))
     update_sec_fillings(symbol="FB", filling_type="txt", form_type="8-K")
     import os
